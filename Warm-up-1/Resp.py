fdd43ab53ea764bfee5acbd4fa88e4cf7cf377cf
Supernova <supernova313.313z@gmail.com>
2023-01-11 04:19:20 +0330

    alpha version of bot

--- Warm-up-1/Resp.py
+++ Warm-up-1/Resp.py
@@ -1,4 +1,89 @@
-def responses(text, state):
+def responses(text, state, user_data, **args):
+	if state == 2:
+		if text not in user_data:
+			user_data[text] = {}
+			user_data["last_command"] = 0
+			user_data["events"].append(text)
+			return "The desired event was created."
+		else:
+			return "There is an event with this name. Please enter another name:"
+
+	# ----------------------------------------------------------------------------
+	elif state == 3:
+		if text in user_data:
+			user_data["last_command"] = 0
+			user_data["Head"] = text
+			return "You have successfully entered the event."
+		else:
+			return "The entered name is not correct. Please try again:"
+
+	# ----------------------------------------------------------------------------
+	elif state == 4:
+		print(user_data[user_data["Head"]])
+		if text == "exit":
+			user_data["last_command"] = 3
+			head = user_data["Head"]
+			return f"Back to event {head}"
+		elif text in user_data[user_data["Head"]]:
+			return "This name already exists.\nPlease enter another name or type \"exit\" to exit:"
+		else:
+			user_data[user_data["Head"]][text] = 0
+			return "User added seccessfully. \nPlease enter another name or type \"exit\" to exit:"
+
+	# ----------------------------------------------------------------------------
+	elif state == 5:
+		if text == "exit":
+			user_data["last_command"] = 3
+			head = user_data["Head"]
+			return f"Back to event {head}"
+		else:
+			user_data["last_command"] = 5.1
+			return "Please enter the transaction amount:"
 
 
-	return 0
+	# ----------------------------------------------------------------------------
+	elif state == 5.1:
+		if text == "exit":
+			user_data["last_command"] = 3
+			head = user_data["Head"]
+			return f"Back to event {head}"
+		else:
+			user_data["amount"] = float(text)
+			user_data["last_command"] = 5.2
+			# cheak
+			names = list(user_data[user_data["Head"]].keys())
+			return f"Please enter the names of the participants in this transaction, separated by a '-'.\nNames of all members:\n{names}"
+
+
+	# ----------------------------------------------------------------------------
+	elif state == 5.2:
+		if text == "exit":
+			user_data["last_command"] = 3
+			head = user_data["Head"]
+			return f"Back to event {head}"
+		else:
+			user_data["last_command"] = 5.3
+			user_data["Users"] = text.split("-")
+			return "Please enter the name of the buyer of this transaction:"
+
+
+	# ----------------------------------------------------------------------------
+	elif state == 5.3:
+		if text == "exit":
+			user_data["last_command"] = 3
+			head = user_data["Head"]
+			return f"Back to event {head}"
+		else:
+			user_data["last_command"] = 3
+			share = user_data["amount"]/len(user_data["Users"])
+			user_data[user_data["Head"]][text] += user_data["amount"]
+			for i in user_data["Users"]:
+				user_data[user_data["Head"]][i] -= share
+			user_data["amount"] = 0
+			user_data["Users"] = []
+			return "The transaction was added successfully."
+
+
+	# ----------------------------------------------------------------------------
+	else:
+		return "We could not understand your message."
--- Warm-up-1/main.py
+++ Warm-up-1/main.py
@@ -1,29 +1,106 @@
 import Constant as keys
 import Resp as R
+from os import system
 from telegram.ext import *
 from telegram import KeyboardButton, ReplyKeyboardMarkup
 from telegram import InlineKeyboardButton as IKB, InlineKeyboardMarkup as IKM, CallbackQuery
 
 # ======================================================================
 def start_command(update, context):
+	data = context.user_data
+	data["last_command"] = 0
+	data["events"] = []
 	update.message.reply_text("Hello\n Welcome to my Bot\n")
+# ----------------------------------------------------------------------
 
 
 def help_command(update, context):
+	data = context.user_data
+	data["last_command"] = 1
 	update.message.reply_text("Whats happand my frind?!\nIf you have questions about the operation of the robot, you can read the description file.")
+# ----------------------------------------------------------------------
 
 
 def new_event(update, context):
 	update.message.reply_text("To create a new event, first choose a name:")
+	data = context.user_data
+	data["last_command"] = 2
+# ----------------------------------------------------------------------
+
+
+def go_to_event(update, context):
+	data = context.user_data
+	data["last_command"] = 3
+	update.message.reply_text("Please enter the destination event name.")
+# ----------------------------------------------------------------------
+
+
+def add_member(update, context):
+	data = context.user_data
+	if data.get("Head"):
+		data["last_command"] = 4
+		update.message.reply_text("Please enter the name of the event member:")
+	else:
+		update.message.reply_text("Please enter an event first.")
+# ----------------------------------------------------------------------
+
+
+def Add_transaction(update, context):
+	data = context.user_data
+	# should chan this
+	if data.get("Head"):
+		data["last_command"] = 5
+		update.message.reply_text("Please enter the name of the transaction:")
+	else:
+		update.message.reply_text("Please enter an event first.")
+# ----------------------------------------------------------------------
+
+
+def show_state(update, context):
+	data = context.user_data
+	# should chan this
+	if data.get("Head"):
+		data["last_command"] = 6
+		users = list(data[data["Head"]].keys())
+		states = list(data[data["Head"]].values())
+		text = "status of accounts:"
+		for ind, i in enumerate(users):
+			text += f"{i} = {states[ind]}\n"
+		update.message.reply_text(text)
+	else:
+		update.message.reply_text("Please enter an event first.")
+# ----------------------------------------------------------------------
+
+
+def send_state(update, context):
+	data = context.user_data
+	# should chan this
+	if data.get("Head"):
+		data["last_command"] = 7
+		users = list(data[data["Head"]].keys())
+		states = list(data[data["Head"]].values())
+		text = "status of accounts:"
+		for ind, i in enumerate(users):
+			text += f"{i} = {states[ind]}\n"
+		with open("status_accounts.txt", "a+") as f:
+			f.write(text)
+			update.message.reply_document(f, caption="status of accounts")
+		system("rm status_accounts.txt")
+	else:
+		update.message.reply_text("Please enter an event first.")
+# ----------------------------------------------------------------------
 
 
 def message_handler(update, context):
     data = context.user_data
-    # data["last_command"] = "message"
     text = str(update.message.text)
-    result = R.responses(text, state)
+    state = data["last_command"]
+    result = R.responses(text, state, data)
     update.message.reply_text(result)
 
+
+def error(update, context):
+    print(f"Update {update} ==> {context.error}")
 
 # ======================================================================
 def main():
@@ -34,6 +111,15 @@ def main():
 	# add handlers
 	dp.add_handler(CommandHandler("start", start_command))
 	dp.add_handler(CommandHandler("help", help_command))
+	dp.add_handler(CommandHandler("new_event", new_event))
+	dp.add_handler(CommandHandler("go_to_event", go_to_event))
+	dp.add_handler(CommandHandler("add_member", add_member))
+	dp.add_handler(CommandHandler("Add_transaction", Add_transaction))
+	dp.add_handler(CommandHandler("show_state", show_state))
+	dp.add_handler(CommandHandler("send_state", send_state))
+	# ------------
+	dp.add_handler(MessageHandler(Filters.text, message_handler))
+	dp.add_error_handler(error)
 
 
 	# start bot
