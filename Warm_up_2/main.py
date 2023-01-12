9f43da69f2987986b0507c4e3b688379817e3d71
Supernova <supernova313.313z@gmail.com>
2023-01-12 11:16:47 +0330

    version alpha

--- Warm_up_2/main.py
+++ Warm_up_2/main.py
@@ -1,6 +1,205 @@
 import Constant as keys
+import responses as R
+from telegram.ext import *
+from telegram import KeyboardButton, ReplyKeyboardMarkup
+from telegram import InlineKeyboardButton as IKB, InlineKeyboardMarkup as IKM, CallbackQuery
+from os import system
+
+# ---------------------------------------------------------------------------------------------
+
+def start(update, context):
+	user_name = update.effective_user['first_name']
+	user_dict = context.user_data
+	user_dict["Where"] = "__Home__"
+	user_dict["State"] = 0
+	user_dict["temp"] = []
+	kb = [[KeyboardButton("/add_member"), KeyboardButton("/share")], [KeyboardButton("/transaction")], 
+		  [KeyboardButton("/Checkout"), KeyboardButton("/data_file")], [KeyboardButton("/back_to_home")]]
+	kb_m = ReplyKeyboardMarkup(kb, one_time_keyboard=True)
+	user_dict["KeyboardButton"] = kb_m
+	update.message.reply_text(f"🆔 Hello {user_name}.\n🤖 Welcome to Money Management Robot.💯\n"
+							   "📦 You can use the following command to start.\n🔰 /add_event\n🔰 /go_to_event\n"
+							   "❗️ Run the /help command to get help.", quote=True)
+
+"""/////////////////////////////////////////////////////////////////////////////////////////////"""
+
+def help_u(update, context):
+	user_dict = context.user_data
+	user_dict["State"] = 1
+	user_dict["temp"] = []
+	update.message.reply_text("hello", quote=True)
+
+"""/////////////////////////////////////////////////////////////////////////////////////////////"""
+
+def messages(update, context):
+	user_dict = context.user_data
+	text = str(update.message.text)
+	result = R.process(text, user_dict, user_dict["State"])
+	if user_dict["State"] == -1:
+		kb_m = user_dict["KeyboardButton"]
+		update.message.reply_text(result, quote=True, reply_markup=kb_m)
+	else:
+		update.message.reply_text(result, quote=True)
+
+"""/////////////////////////////////////////////////////////////////////////////////////////////"""
+# remove this
+def error(update, context):
+	print(f"{update} \n {context.error}")
+
+"""/////////////////////////////////////////////////////////////////////////////////////////////"""
+
+def add_event(update, context):
+	user_dict = context.user_data
+	user_dict["State"] = 2
+	user_dict["temp"] = []
+	update.message.reply_text("Please enter the name of the event:", quote=True)
+
+"""/////////////////////////////////////////////////////////////////////////////////////////////"""
+
+def go_to_event(update, context):
+	user_dict = context.user_data
+	if len(user_dict) > 3:
+		user_dict["State"] = 3
+		user_dict["temp"] = []
+		update.message.reply_text("Please enter the name of the event you want to enter:", quote=True)
+	else:
+		update.message.reply_text("❌ Please add an event first.\n⭕ Use this command to add:\n🔰 /add_event")
+
+"""/////////////////////////////////////////////////////////////////////////////////////////////"""
+
+def all_event(update, context):
+	user_dict = context.user_data
+	user_dict["State"] = 4
+	user_dict["temp"] = []
+	events = list(user_dict.keys())
+	events.remove("temp")
+	events.remove("Where")
+	events.remove("State")
+	if len(events) > 0:
+		result = "📃 Events List:\n"
+		for i in events:
+			result += f"📦 {i}\n"
+		update.message.reply_text(result, quote=True)
+	else:
+		update.message.reply_text("❌ No events have been added yet.", quote=True)
+
+"""/////////////////////////////////////////////////////////////////////////////////////////////"""
+
+def back_to_home(update, context):
+	if context.user_data["Where"] != "__Home__":
+		context.user_data["Where"] = "__Home__"
+		context.user_data["State"] = 0
+		context.user_data["temp"] = []
+		update.message.reply_text("🏠 You have returned to the Home page.\n🛟 Suggested commands:\n"
+								  "🔰 /add_event\n🔰 /all_event\n🔰 /go_to_event")
+	else:
+		update.message.reply_text("🏠 You are in home already.\n🛟 Suggested commands:\n"
+								  "🔰 /add_event\n🔰 /all_event\n🔰 /go_to_event")
+
+"""/////////////////////////////////////////////////////////////////////////////////////////////"""
+
+def add_member(update, context):
+	user_dict = context.user_data
+	user_dict["State"] = 5
+	user_dict["temp"] = []
+	update.message.reply_text("Please enter a name or enter the 'exit' to exit:", quote=True)
+
+"""/////////////////////////////////////////////////////////////////////////////////////////////"""
+
+def share(update, context):
+	user_dict = context.user_data
+	user_dict["State"] = -1
+	event_name = user_dict["Where"]
+	kb_m = user_dict["KeyboardButton"]
+	if len(user_dict[event_name]) > 0:
+		names = list(user_dict[event_name].keys())
+		moneys = list(user_dict[event_name].values())
+		result = f"📋 Status of accounts in event {event_name}:\n"
+		for ind, i in enumerate(names):
+			result += f"🆔 {i} = {moneys[ind]}\n"
+		update.message.reply_text(result, quote=True, reply_markup=kb_m)
+	else:
+		update.message.reply_text("⛔️ Please enter the names of the participants first.\n"
+								  "⭕ Use this command to add member:\n🔰 /add_member", quote=True, reply_markup=kb_m)
+
+"""/////////////////////////////////////////////////////////////////////////////////////////////"""
+
+def transaction(update, context):
+	user_dict = context.user_data
+	event_name = user_dict["Where"]
+	user_dict["temp"] = []
+	kb_m = user_dict["KeyboardButton"]
+	if len(user_dict[event_name]) > 0:
+		user_dict["State"] = 6
+		update.message.reply_text("Please enter the name of the transaction:", quote=True)
+	else:
+		update.message.reply_text("⛔️ Please enter the names of the participants first.\n"
+								  "⭕ Use this command to add member:\n🔰 /add_member", quote=True, reply_markup=kb_m)		
 
 
-if __name__ == "__main__":
-	pass
+"""/////////////////////////////////////////////////////////////////////////////////////////////"""
 
+def Checkout(update, context):
+	user_dict = context.user_data
+	event_name = user_dict["Where"]
+	user_dict["temp"] = []
+	kb_m = user_dict["KeyboardButton"]
+	if len(user_dict[event_name]) > 0:
+		user_dict["State"] = 7
+		update.message.reply_text("Who wants to pay?", quote=True)
+	else:
+		update.message.reply_text("⛔️ Please enter the names of the participants first.\n"
+								  "⭕ Use this command to add member:\n🔰 /add_member", quote=True, reply_markup=kb_m)		
+
+"""/////////////////////////////////////////////////////////////////////////////////////////////"""
+
+def data_file(update, context):
+	user_dict = context.user_data
+	user_dict["State"] = -1
+	event_name = user_dict["Where"]
+	kb_m = user_dict["KeyboardButton"]
+	if len(user_dict[event_name]) > 0:
+		names = list(user_dict[event_name].keys())
+		moneys = list(user_dict[event_name].values())
+		result = f"Status of accounts in event {event_name}:\n"
+		for ind, i in enumerate(names):
+			result += f"{i} = {moneys[ind]}\n"
+		with open("state.txt", "a+") as txt:
+			txt.write(result)
+		update.message.reply_document(open("state.txt", "r+"), caption=f"📋 Status of accounts in event {event_name}:\n", quote=True, reply_markup=kb_m)
+	else:
+		update.message.reply_text("⛔️ Please enter the names of the participants first.\n"
+								  "⭕ Use this command to add member:\n🔰 /add_member", quote=True, reply_markup=kb_m)
+
+# ---------------------------------------------------------------------------------------------
+
+def main():
+	updater = Updater(keys.api, use_context=True)
+	dp = updater.dispatcher
+
+	dp.add_handler(CommandHandler("start", start))
+	dp.add_handler(CommandHandler("help", help_u))
+	dp.add_handler(CommandHandler("add_event", add_event))
+	dp.add_handler(CommandHandler("go_to_event", go_to_event))
+	dp.add_handler(CommandHandler("all_event", all_event))
+	dp.add_handler(CommandHandler("back_to_home", back_to_home))
+	# remove this
+	dp.add_error_handler(error)
+	dp.add_handler(MessageHandler(Filters.text, messages))
+
+	updater.start_polling(0.25)
+	print("bot started ...")
+	updater.idle()
+
+main()
+# my bot id = @money_management2_bot
+
+"""
+user_dict = {
+	Where : str             (__Home__, trip, ...)
+	State : int             (0, 1, ...)
+	temp  : list            (amount, names, ...)
+	_Event_name_ : dict     ("ali" : 0, "ahmad" : 500)
+}
+"""
+""" ⛔️⭕✅🌀❌💳🤖📦🔰🆔❗️🛟💯⛺️🏕💸👥 🚀📃📋🏠"""
---
+++ Warm_up_2/responses.py
@@ -0,0 +1,90 @@
+
+def process(text: str, user_dict: dict, state: int):
+	# =======================================================================
+	if state == 2:
+		if text not in user_dict and text != "__Home__":
+			user_dict[text] = {}
+			user_dict["State"] = 0
+			return str("Event added successfully. ✅\n"
+					 "🌀 Using the following commands, you can switch between events and view the available events:" +
+					 "\n🔰 /go_to_event\n🔰 /all_event")
+		else:
+			return "⛔️ This name cannot be used.\n⭕️ Please enter another name:"
+
+	# =======================================================================
+
+	elif state == 3:
+		if text in user_dict and text not in ["temp", "State", "Where", "__Home__"]:
+			user_dict["Where"] = text
+			user_dict["State"] = -1
+			user_dict["temp"] = []
+			return f"✅ You have entered event {text}.\n🌀 Please select a command from the menu:"
+		else:
+			return str("⛔️ The entered name is wrong.\n⭕️ Please enter another name or use the command below to view event names:\n" + 
+					 "🔰 /all_event")
+
+	# =======================================================================
+
+	elif state == 5:
+		if text == "exit":
+			user_dict["State"] = -1
+			event_name = user_dict["Where"]
+			return f"✅ Back to event {event_name}.\n🌀 Please select a command from the menu:"
+		else:
+			event_name = user_dict["Where"]
+			user_dict[event_name][text] = 0
+			return f"✅ Name added seccessfully.\n🌀 Please enter a name or enter the 'exit' to exit:"
+
+	# =======================================================================
+	
+	elif state == 6:
+		user_dict["State"] = 6.1
+		return "Please enter the transaction amount:"
+
+	elif state == 6.1:
+		user_dict["State"] = 6.2
+		user_dict["temp"].append(int(float(text)))
+		names = list(user_dict[user_dict["Where"]].keys())
+		result = "Enter the names of the participants in this transaction:\nWrite each name on one line.\nThe names of all the people in this event are:\n"
+		for i in names:
+			result += f"{i}\n"
+		return result
+
+	elif state == 6.2:
+		user_dict["State"] = 6.3
+		user_dict["temp"].append(text.split("\n"))
+		return "Who paid for this transaction?"
+
+	elif state == 6.3:
+		user_dict["State"] = -1
+		event_name = user_dict["Where"]
+		user_dict[event_name][text] += user_dict["temp"][0]
+		share = user_dict["temp"][0]/len(user_dict["temp"][1])
+		for i in user_dict["temp"][1]:
+			user_dict[event_name][i] -= share
+		user_dict["temp"] = []
+		return "✅ Transaction added successfully.\n🌀 Please select a command from the menu:"
+
+	# =======================================================================
+
+	elif state == 7:
+		user_dict["State"] = 7.1
+		user_dict["temp"].append(text)
+		return "Who receives the money?"
+
+	elif state == 7.1:
+		user_dict["State"] = 7.2
+		user_dict["temp"].append(text)
+		return "How much is the deposit amount?"
+
+	elif state == 7.2:
+		user_dict["State"] = -1
+		user_dict[user_dict["Where"]][user_dict["temp"][0]] += int(float(text))
+		user_dict[user_dict["Where"]][user_dict["temp"][1]] -= int(float(text))
+		user_dict["temp"] = []
+		return "✅ Checkout was done successfully.\n🌀 Please select a command from the menu:"
+
+	# =======================================================================
+
+	else:
+		return "Error:\n❌   Please enter a valid message."
